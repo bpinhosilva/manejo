debb68f4265f1d0d3cd7d9eed0ded95423c969b3
Bruno Pinho <bruno@debian-bp>
2015-05-01 19:01:20 -0300

    Correção encolher menu responsivo no clique
    Efetuada a modificação no menu responsivo em smartphones para
    encolher as opções ao clicar em algum elemento.

--- js/manejo.js
+++ js/manejo.js
@@ -2,6 +2,18 @@ var tab = ""; // define que aba sera exibida na pagina
 
 $(document).ready(function() {
   
+  // para recolher menu responsivo quando clicar na opcao
+  
+  $('.nav a').on('click', function(){
+    // verifica o tamanho da tela para eliminar bugs
+    // em desktop e tablets no menu
+    if (window.innerWidth <= 768) {
+      $(".navbar-toggle").click();
+    }
+    //$(".navbar-collapse").collapse('hide');
+  });
+
+
   $("#manejo").click(function() {
     $("#" + tab).parent().toggleClass("active");
     tab = "manejo";
