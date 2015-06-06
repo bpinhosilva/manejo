013e57f7869e3b1ef90609abb725dba3ab5244dd
Bruno Pinho <bpinhosilva@gmail.com>
2015-06-06 17:01:41 -0300

    Correção login
    Efetuada a requisição ao servidor no evento de login de usuário

--- js/login.js
+++ js/login.js
@@ -20,14 +20,23 @@ var login = (function(){
       c1: userLogin,
       c2: userPassword
     }, function (data) {
-      alert(data);      
+      console.log("retorno");
+      eval(data); 
+      var c0, c1, c2;
+
+      switch(c0) {
+        case 4: // successful login
+          window.location.href = "http://104.236.14.87/ManejoNovo/home.html";
+          break;
+      }     
     });
   };
 
   // public members
   return {
     init: function () {
-      $(".form-signin").submit( function () {
+      $(".form-signin").submit( function (e) {
+        e.preventDefault();
         var btnType = $(document.activeElement).attr('id');
         
         switch(btnType) {
